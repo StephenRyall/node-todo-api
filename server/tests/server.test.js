8e87219ee66a73a812def9b088b35d5024964313
Steven <stevenryall812@gmail.com>
2018-05-15 17:29:51 +0100

    Test commit

---
+++ server/tests/server.test.js
@@ -0,0 +1,54 @@
+const expect = require("expect");
+const request = require("supertest");
+
+const { app } = require("./../server");
+const { Todo } = require("./../models/todo");
+
+beforeEach(done => {
+  Todo.remove({}).then(() => done()); //to ensure that even if there are existing objects inside the DB
+}); //they will be clear so the test will pass
+
+describe("POST /todos", () => {
+  //decribe is a group of checks
+  it("should create a new todo", done => {
+    let text = "Test todo text";
+
+    request(app)
+      .post("/todos")
+      .send({ text })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(1);
+            expect(todos[0].text).toBe(text);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it("should not create todo with invalid body data", done => {
+    request(app)
+      .post("/todos")
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find()
+          .then(todos => {
+            expect(todos.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+});
