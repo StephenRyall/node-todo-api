d71c9df065ec0343ae55b26b83a35281f18f9f8e
Steven <stevenryall812@gmail.com>
2018-05-17 17:56:16 +0100

    Test the DELETE /todos/:id route'

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,42 @@ describe("GET /todos/:id", () => {
       .end(done);
   });
 });
+
+describe("DELETE /todos/:id", () => {
+  //good example of a nested test with slight complexity
+  it("should remove a todo", done => {
+    let hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId)
+          .then(todo => {
+            expect(todo).toNotExist();
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+  it("should return 404 if todo not found", done => {
+    let hexId = new ObjectID().toHexString;
+    request(app)
+      .delete(`/todo/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+  it("should retun if object id is invalid", done => {
+    request(app)
+      .delete("/todos/123abc")
+      .expect(404)
+      .end(done);
+  });
+});
